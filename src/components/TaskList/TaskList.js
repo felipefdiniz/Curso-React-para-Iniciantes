c1d4bd0dfcc6e9e5e77dc09b3c0f4a8c342f4a9a
felipefdiniz <43547372+felipefdiniz@users.noreply.github.com>
2021-02-19 10:09:44 -0300

    Initial commit

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,49 @@
+import PropTypes from "prop-types";
+import "./tasklist.css";
+
+import TaskItem from "../TaskItem/TaskItem.js";
+
+export default function TaskList({
+  title,
+  taskState,
+  onAddTask,
+  tasks,
+  onTaskUpdate,
+  onDeleteTask
+}) {
+  const addTask = () => {
+    onAddTask("Nova tarefa", taskState);
+  };
+
+  return (
+    <div className="tasklist">
+      <div className="title">{title}</div>
+      <div className="content">
+        {tasks.map((task) => {
+          return (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              taskState={task.state}
+              onTaskUpdate={onTaskUpdate}
+              onDeleteTask={onDeleteTask}
+            />
+          );
+        })}
+        {tasks.length === 0 && <div className="empty-list">Lista Vazia</div>}
+        <button onClick={addTask} className="btn">
+          Adicionar tarefa
+        </button>
+      </div>
+    </div>
+  );
+}
+
+TaskList.PropTypes = {
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired,
+  onTaskUpdate: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired
+};
